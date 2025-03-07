7998167aa70d260719c3896e9c8b103c0ba54ec7
tamgiao <nambhhe176453@fpt.edu.vn>
2025-03-07 11:28:31 +0700

    Update TestHistory.api.js

--- src/api/TestHistory.api.js
+++ src/api/TestHistory.api.js
@@ -2,7 +2,7 @@ import apiClient from "./apiClient";
 
 export const submitTest = async (userId, testId, answers, userInfo) => {
   try {
-    const response = await apiClient.post(`/test-history/submit/67a0374b7ad0db88c8b251c0/${testId}`, { answers, userInfo }); 
+    const response = await apiClient.post(`/test-history/submit/67c732358b25ae48db7559a4/${testId}`, { answers, userInfo }); 
     return response.data; 
   } catch (error) {
     console.error("Error submitting test:", error);
